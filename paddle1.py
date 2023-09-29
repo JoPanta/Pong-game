94754a5951f4674108bdb101337031e2c1e1bf21
JoPanta <jopantaleao@gmail.com>
2023-09-29 23:30:07 +0100

    Update paddle1.py

--- paddle1.py
+++ paddle1.py
@@ -3,16 +3,13 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, xcor, ycor):
         super().__init__()
-        self.hideturtle()
-        self.speed(0)
         self.shape("square")
         self.penup()
         self.turtlesize(stretch_wid=5, stretch_len=1)
         self.color("white")
-        self.setposition(350, 0)
-        self.showturtle()
+        self.setposition(xcor, ycor)
 
     def move_up(self):
         new_y = self.ycor() + 20
