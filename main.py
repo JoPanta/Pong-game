9384ee25a44ba2fd73fe7f1c873ad8d056f5655d
JoPanta <jopantaleao@gmail.com>
2023-09-29 23:30:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from turtle import Turtle, Screen
 from paddle1 import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 
-right_paddel = Paddle()
-
+right_paddle = Paddle(350, 0)
+left_paddle = Paddle(-350, 0)
+ball = Ball()
 
 screen.listen()
-screen.onkeypress(right_paddel.move_up, "Up")
-screen.onkeypress(right_paddel.move_down, "Down")
+screen.onkeypress(right_paddle.move_up, "Up")
+screen.onkeypress(right_paddle.move_down, "Down")
+screen.onkeypress(left_paddle.move_up, "w")
+screen.onkeypress(left_paddle.move_down, "s")
+
+game_is_on = True
 
 
+while game_is_on:
+    time.sleep(0.05)
+    screen.update()
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
 
 
